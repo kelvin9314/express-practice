df12a86a60395f53b60c0d29280e499c74b30ab0
kelvin <kelvin@program.com.tw>
2019-04-22 13:39:54 +0800

    Route Parameters

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get('/api/courses', (req, res) => {
   res.send([1, 2, 3]);
 });
 
+app.get('/api/courses/:id', (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+  res.send(req.query);
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
