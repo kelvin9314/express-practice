ca4436cf88359764ada07d49edbde042d31ecb04
kelvin <kelvin@program.com.tw>
2019-04-22 13:50:58 +0800

    Handling HTTP GET Requests

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
 
+const courses = [{ id: 1, name: 'maths' }, { id: 2, name: 'chinese' }, { id: 3, name: 'english' }];
+
 app.get('/', (req, res) => {
   res.send('Hello world!!!');
 });
 
 app.get('/api/courses', (req, res) => {
   // in real env , get the list of data from database and return them
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-  res.send(req.params.id);
-});
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The courses with the given ID was not found.');
 
-app.get('/api/posts/:year/:month', (req, res) => {
-  res.send(req.query);
+  res.send(course);
 });
 
 const port = process.env.PORT || 8080;
